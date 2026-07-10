5f3215521bf413acbe2d4f57e5025506eac9da58
JerrettDavis <mxjerrett@gmail.com>
2026-07-10 06:56:39 -0500

    refactor(proxy): isolate output verbosity policy

--- headroom/proxy/output_shaper.py
+++ headroom/proxy/output_shaper.py
@@ -86,7 +86,6 @@ _OPENAI_RESPONSES_OUTPUT_ITEM_TYPES = frozenset(
 
 _replace_or_append_steering_block = replace_or_append_steering_block
 
-
 class TurnKind(Enum):
     """Structural classification of the latest conversation turn."""
 
